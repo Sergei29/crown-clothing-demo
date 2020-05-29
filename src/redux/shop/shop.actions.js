bcdf821f0fbb1993603564344a29c4cfe5f5f360
Sergejs Basangovs <sergejs.basangovs@ingresso.co.uk>
2020-05-29 13:19:12 +0100

    redux-saga installed, and used with shop reducer

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,10 +1,6 @@
 import shopActionTypes from "./shop.types";
-import {
-	firestore,
-	convertCollectionsSnapshotToMap,
-} from "../../firebase/firebase.utils";
 
-export const fetchCollectionsStart = (params) => ({
+export const fetchCollectionsStart = () => ({
 	type: shopActionTypes.FETCH_COLLECTIONS_START,
 });
 
@@ -17,26 +13,3 @@ export const fetchCollectionsFailure = (errorMessage) => ({
 	type: shopActionTypes.FETCH_COLLECTIONS_FAILURE,
 	payload: errorMessage,
 });
-
-export const fetchCollectionsStartAsync = () => {
-	return (dispatch) => {
-		// 1. dispatch fetch collection start
-		dispatch(fetchCollectionsStart());
-		// 2. fetch the data.then dispatch the action success OR fail
-		firestore
-			.collection("collections")
-			.get()
-			.then((snapshot) => {
-				if (snapshot.empty) {
-					throw new Error("Cannot fetch the data.");
-				}
-				const collectionsMap = convertCollectionsSnapshotToMap(
-					snapshot
-				);
-				dispatch(fetchCollectionsSuccess(collectionsMap));
-			})
-			.catch((error) => {
-				dispatch(fetchCollectionsFailure(error.message));
-			});
-	};
-};
---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,36 @@
+import { takeLatest, call, put } from "redux-saga/effects";
+import ShopActionTypes from "./shop.types";
+import {
+	firestore,
+	convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
+import {
+	fetchCollectionsSuccess,
+	fetchCollectionsFailure,
+} from "./shop.actions";
+
+// action type listener gen function
+export function* fetchCollectionsAsync() {
+	const collectionRef = firestore.collection("collections");
+
+	try {
+		const snapshot = yield collectionRef.get();
+
+		const collectionsMap = yield call(
+			convertCollectionsSnapshotToMap,
+			snapshot
+		);
+
+		yield put(fetchCollectionsSuccess(collectionsMap));
+	} catch (error) {
+		yield put(fetchCollectionsFailure(error.message));
+	}
+}
+
+// subscribe to action type
+export function* fetchCollectionsStart() {
+	yield takeLatest(
+		ShopActionTypes.FETCH_COLLECTIONS_START,
+		fetchCollectionsAsync
+	);
+}
